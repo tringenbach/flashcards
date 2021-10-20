13f6f7c499759ad871e94c65a5760c2a478580fc
Tim Ringenbach <tim.ringenbach@gmail.com>
2021-10-19 21:53:14 -0500

    Add home link

--- src/App.js
+++ src/App.js
@@ -12,15 +12,25 @@ import {
 function App() {
   return (
     <Router>
+      <Route path="/:subpage">
+        <div style={{ position: 'absolute', backgroundColor: 'white', padding: '8px'}}>
+          <Link to="/">Home</Link>
+        </div>
+      </Route>
+
       <Switch>
         <Route exact path="/">
           <ul>
             <li><Link to="/addition">Addition ➕</Link></li>
             <li><Link to="/subtraction">Subtraction ➖</Link></li>
             <li><Link to="/sight-words-pk">Sight Words Pre-K</Link></li>
-
+            <li><Link to="/sight-words-k">Sight Words K</Link></li>
+            <li><Link to="/sight-words-1">Sight Words 1</Link></li>
+            <li><Link to="/sight-words-2">Sight Words 2</Link></li>
+            <li><Link to="/sight-words-3">Sight Words 3</Link></li>
           </ul>
         </Route>
+
         <Route path="/addition">
           <MathProblem mode="addition" />
         </Route>
@@ -29,6 +39,18 @@ function App() {
         </Route>
         <Route path="/sight-words-pk" >
           <SightWords level="pk" />
+        </Route>
+        <Route path="/sight-words-k" >
+          <SightWords level="k" />
+        </Route>
+        <Route path="/sight-words-1" >
+          <SightWords level="1" />
+        </Route>
+        <Route path="/sight-words-2" >
+          <SightWords level="2" />
+        </Route>
+        <Route path="/sight-words-3" >
+          <SightWords level="3" />
         </Route>
       </Switch>
 
