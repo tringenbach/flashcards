95bf561b4d918bc00b4ccedd7837895f2f886a2a
Tim Ringenbach <tim.ringenbach@gmail.com>
2021-10-19 19:15:58 -0500

    Add home page, Subtraction

--- src/App.js
+++ src/App.js
@@ -1,115 +1,34 @@
-import { useState, useRef } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import right from './audio/right.mp3';
-import wrong from './audio/wrong.mp3'
+import MathProblem from "./MathProblem";
 
-
-function getAdditionProblems() {
-  const rv = [];
-  for (let i = 0; i <= 10; i++) {
-    for (let j = 0; j <= 10; j++) {
-      rv.push({
-        problem: `${i} + ${j}`,
-        solution: i + j
-      });
-    }
-  }
-  return rv;
-}
-
-function getRandomProblem(problems) {
-  const min = 0;
-  const max = problems.length;
-  const index = Math.floor(Math.random() * (max - min) + min); 
-  return problems[index];
-}
-
-function RightAudios({count}) {
-  const [ times, setTimes ] = useState(count);
-  return <audio onEnded={(e) => {
-    if (times > 1) {
-      e.target.play();
-      setTimes(times - 1);
-    }
-  }} autoplay="true" src={ right } /> 
-}
-
-function RightAudio() {
-  return <audio autoplay="true" src={ right } />
-}
-function WrongAudio() {
-  return <audio autoplay="true" src={ wrong } />
-}
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
-  const [ streak, setStreak ] = useState(0);
-  const problems = getAdditionProblems();
-  const [ problem, setProblem ] = useState(getRandomProblem(problems));
-  const [ answered, setAnswered ] = useState(false);
-  const [ userSolution, setUserSolution ] = useState(null);
-
-  const correctAnswer = (answered && userSolution == problem.solution);
-  const incorrectAnswer = (answered && userSolution != problem.solution)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        { streak > 0 &&
-          <p>
-            Streak: {streak} {"✅".repeat(streak)}
-          </p>
-        }
-        <p style={{ fontSize: '40px'}}>
-          {problem.problem} = 
-          { !answered &&
-          <form style={{ display: 'inline' }} onSubmit={() => {
-            setAnswered(true);
-            if (userSolution == problem.solution) setStreak(streak + 1);
-            else setStreak(0);
-          }}>
-            <span> </span><input autoFocus type="text" size={2} style={{ fontSize: '40px'}} onInput={e => setUserSolution(e.target.value)}/>
-          </form>
-          }
-          { answered && <span> {userSolution}</span>}
-          { correctAnswer && <span> ✅<RightAudios count={ streak } /></span>}
-          { incorrectAnswer && <span> ❌<WrongAudio /></span>}
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <ul>
+            <li><Link to="/addition">Addition ➕</Link></li>
+            <li><Link to="/subtraction">Subtraction ➖</Link></li>
+            <li><Link to="/site-words-pk">Site Words Pre-K</Link></li>
 
-          { incorrectAnswer && (
-            <span>
-              <button style={{ marginLeft: '20px' }} onClick={() => setAnswered(false)}>Try Again</button>
-            </span>
-        )}
+          </ul>
+        </Route>
+        <Route path="/addition">
+          <MathProblem mode="addition" />
+        </Route>
+        <Route path="/subtraction">
+          <MathProblem mode="subtraction" />
+        </Route>
+      </Switch>
 
-        { correctAnswer && (
-          <button style={{ marginLeft: '20px' }} onClick={() => {
-            setProblem(getRandomProblem(problems));
-            setAnswered(false);
-            setUserSolution(null);
-          }}>Next</button>
-        )}
-
-        </p>
-        { incorrectAnswer &&
-        <p>
-        The answer was {problem.solution}
-        </p>
-        }
-
-      </header>
-      <main>
-        <div>
-          Right ✅<audio controls src={ right } />
-        </div>
-        <div>
-          Wrong ❌<audio controls src={ wrong } />
-        </div>
-        <div>
-          Times up! ⏰ <audio controls src={ wrong } />
-        </div>
-      </main>
-    </div>
+    </Router>
   );
 }
 
---
+++ src/MathProblem.js
@@ -0,0 +1,127 @@
+import { useState, useRef } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import right from './audio/right.mp3';
+import wrong from './audio/wrong.mp3'
+
+
+function getAdditionProblems(mode ) {
+  const rv = [];
+  for (let i = 0; i <= 10; i++) {
+    for (let j = 0; j <= 10; j++) {
+      if (mode === 'addition') {
+        rv.push({
+          problem: `${i} + ${j}`,
+          solution: i + j
+        });
+      } else if (mode === "subtraction") {
+        const m1 = Math.max(i, j);
+        const m2 = Math.min(i, j);
+        const solution = m1 - m2;
+        rv.push({
+          problem: `${m1} - ${m2}`,
+          solution
+        });
+      }
+
+    }
+  }
+  return rv;
+}
+
+function getRandomProblem(problems) {
+  const min = 0;
+  const max = problems.length;
+  const index = Math.floor(Math.random() * (max - min) + min); 
+  return problems[index];
+}
+
+function RightAudios({count}) {
+  const [ times, setTimes ] = useState(count);
+  return <audio onEnded={(e) => {
+    if (times > 1) {
+      e.target.play();
+      setTimes(times - 1);
+    }
+  }} autoplay="true" src={ right } /> 
+}
+
+function RightAudio() {
+  return <audio autoplay="true" src={ right } />
+}
+function WrongAudio() {
+  return <audio autoplay="true" src={ wrong } />
+}
+
+function MathProblem({ mode }) {
+  const [ streak, setStreak ] = useState(0);
+  const problems = getAdditionProblems(mode);
+  const [ problem, setProblem ] = useState(getRandomProblem(problems));
+  const [ answered, setAnswered ] = useState(false);
+  const [ userSolution, setUserSolution ] = useState(null);
+
+  const correctAnswer = (answered && userSolution == problem.solution);
+  const incorrectAnswer = (answered && userSolution != problem.solution)
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        { streak > 0 &&
+          <p>
+            Streak: {streak} {"✅".repeat(streak)}
+          </p>
+        }
+        <p style={{ fontSize: '40px'}}>
+          {problem.problem} = 
+          { !answered &&
+          <form style={{ display: 'inline' }} onSubmit={() => {
+            setAnswered(true);
+            if (userSolution == problem.solution) setStreak(streak + 1);
+            else setStreak(0);
+          }}>
+            <span> </span><input autoFocus type="text" size={2} style={{ fontSize: '40px'}} onInput={e => setUserSolution(e.target.value)}/>
+          </form>
+          }
+          { answered && <span> {userSolution}</span>}
+          { correctAnswer && <span> ✅<RightAudios count={ streak } /></span>}
+          { incorrectAnswer && <span> ❌<WrongAudio /></span>}
+
+          { incorrectAnswer && (
+            <span>
+              <button style={{ marginLeft: '20px' }} onClick={() => setAnswered(false)}>Try Again</button>
+            </span>
+        )}
+
+        { correctAnswer && (
+          <button style={{ marginLeft: '20px' }} onClick={() => {
+            setProblem(getRandomProblem(problems));
+            setAnswered(false);
+            setUserSolution(null);
+          }}>Next</button>
+        )}
+
+        </p>
+        { incorrectAnswer &&
+        <p>
+        The answer was {problem.solution}
+        </p>
+        }
+
+      </header>
+      <main>
+        <div>
+          Right ✅<audio controls src={ right } />
+        </div>
+        <div>
+          Wrong ❌<audio controls src={ wrong } />
+        </div>
+        <div>
+          Times up! ⏰ <audio controls src={ wrong } />
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default MathProblem;
